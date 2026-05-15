703cb5f7f67529fbdd1274dacb59959c41a63e51
piyupi2222-alt <piyupi2222@gmail.com>
2026-05-15 22:36:59 +0900

    add

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
   get "/" => "posts#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :posts
-  post "posts/:id/destroy"=>"posts#destroy"
-  get "posts/:id/destroy"=>"posts#destroy"
+
 end
