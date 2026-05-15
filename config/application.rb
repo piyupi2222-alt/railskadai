898d3b687733694a4f046877ecb5b4f5a5d61fda
piyupi2222-alt <piyupi2222@gmail.com>
2026-05-15 22:03:31 +0900

    add
    Change module name from 'Sct' to 'Myapp' and update configuration settings.

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Sct
+module Myapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.2
@@ -23,5 +23,11 @@ module Sct
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+config.action_controller.permit_all_parameters = true
+    config.time_zone = 'Tokyo'
+    config.i18n.default_locale = :ja
+
+
+    
   end
 end
