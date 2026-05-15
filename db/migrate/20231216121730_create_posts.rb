d38a51690fe71d25025fb2d3ceb9f74bf8b57998
piyupi2222-alt <piyupi2222@gmail.com>
2026-05-15 22:37:47 +0900

    add

--- db/migrate/20231216121730_create_posts.rb
+++ db/migrate/20231216121730_create_posts.rb
@@ -1,12 +1,5 @@
-class CreatePosts < ActiveRecord::Migration[6.1]
+class ChangeDataStartToPost < ActiveRecord::Migration[6.1]
   def change
-    create_table :posts do |t|
-      t.string:"title"
-      t.string:"start"
-      t.string:"finish"
-      t.boolean "allday"
-      t.string "note"
-      t.timestamps
-    end
+    change_column :posts,:start, :date
   end
 end
